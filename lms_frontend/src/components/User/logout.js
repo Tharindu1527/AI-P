0d1318f1e3e78c7c87581d9eeb70ecdd13263e34
Tharindu1527 <157977719+Tharindu1527@users.noreply.github.com>
2025-02-15 21:50:50 +0530

    Delete lms_frontend directory

--- lms_frontend/src/components/User/logout.js
+++
@@ -1,9 +0,0 @@
-function Logout(){
-    localStorage.removeItem('studentLoginStatus')
-    window.location.href='/user-login';
-    return (
-        <div></div>
-    );
-
-}
-export default Logout;
